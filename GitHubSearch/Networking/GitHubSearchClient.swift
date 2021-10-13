6b569e7a498bdade4cb6afd7baa332e9905c218b
David Lee <david.m.lee@ihwic.com>
2021-10-13 20:44:41 +0800

    1. Adding Test 2. Creating GitHubSearchClient

---
+++ GitHubSearch/Networking/GitHubSearchClient.swift
@@ -0,0 +1,12 @@
+//
+//  GitHubSearchClient.swift
+//  GitHubSearch
+//
+//  Created by David on 2021/10/13.
+//
+
+import Foundation
+
+class GitHubSearchClient {
+  
+}
---
+++ GitHubSearchTests/Cases/Networking/GitHubSearchClientTests.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubSearchClientTests.swift
+//  GitHubSearchTests
+//
+//  Created by David on 2021/10/13.
+//
+
+@testable import GitHubSearch
+import XCTest
+
+class GitHubSearchClientTests: XCTestCase {
+  var sut: GitHubSearchClient!
+}
